4c14c7a60750211bbb1fde79ce8822b814580d63
san2025chez <segundousuario@example.com>
2023-06-13 15:20:18 -0300

    purchase-summery

--- src/mercadopago/marcado-pago.js
+++ src/mercadopago/marcado-pago.js
@@ -4,8 +4,14 @@ import { APIs } from '../constants/constants';
 import { useContext } from 'react'
 import { CartCntext2 } from '../context/CartCntext2'
 import { Typography, Container, Grid } from '@material-ui/core';
-import { Link } from '@material-ui/icons';
-import { Button } from '@material-ui/core'
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import CartCounter from '../components/CartCounter/CartCounter'
+import {Link} from 'react-router-dom'
+import swal from 'sweetalert';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 
 import "./mercadopago.scss";
 
@@ -19,6 +25,7 @@ const FORM_ID = 'payment-form';
 
 export default function Product() {
   const { state } = useLocation();
+
 
   console.log(state.user);
 
@@ -72,12 +79,59 @@ export default function Product() {
   ?
   <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '50px' }}>
   <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-    <Typography variant="h2" component="h1" gutterBottom>
-      Bienvenido a Mi Tienda
-    </Typography>
-    <Typography variant="subtitle1" gutterBottom>
-      Realiza tu pago de manera segura con Mercado Pago
-    </Typography>
+  {cart? 
+  <Grid container justifyContent="center" style={{ marginTop: "20px",paddingBottom: "10px"}}>
+  <Card sx={{ minWidth: 275 }} container justifyContent="center">
+  <CardContent justifyContent="center">
+
+<div id="CartList" className="cart container">
+<div className="cart__titulos container">
+  <h6 className="cart__titulos__text">Producto</h6>
+  <h6 className="cart__titulos__text"></h6>
+  <h6 className="cart__titulos__text">Descripción</h6>
+  <h6 className="cart__titulos__text"></h6>
+  <h6 className="cart__titulos__text">Precio</h6>
+</div>
+<div>
+  {cart.map((product) => (
+     <div  key={product.id} className="container cart">
+     <div className="cart__detail container">
+       <div className="cart__img cart__items">
+         <img
+           src={product.img}
+           alt={`img-${product.id}`}
+           className="cart__img-imagen"
+         />
+       </div>
+       <div></div>
+       <div className="cart__items">
+         <h5 className="cart__item"> {product.productName} </h5>
+       </div>
+    <div></div>
+  
+
+       <div className="cart__items">
+         <h6 className="cart__precio"> {product.price} </h6>
+       </div>
+      
+     </div>
+   </div>  
+  )) }  </div>
+<div className="cart__buyTotal container">
+  <h5 className="cart__buyTotal__title ">
+    Total{" "}
+    <span className="cart__buyTotal__title--bold">${totalCompra()}</span>
+  </h5>
+
+
+</div>
+</div>
+</CardContent>
+ </Card>
+ </Grid>
+
+:
+<h3>no hay productos en su carrito</h3>}
     <div >
       <Button
         variant="contained"
