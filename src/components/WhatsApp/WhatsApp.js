7e8576fa5438ebb59c799ea7d733a289441ff7bb
ana <vane2013sanchez@gmail.com>
2023-08-03 13:37:36 -0300

    update-whatssap

--- src/components/WhatsApp/WhatsApp.js
+++ src/components/WhatsApp/WhatsApp.js
@@ -7,7 +7,7 @@ export default function WhatsApp() {
       <a
         target="blank"
       /*   href="https://api.whatsapp.com/send?phone=&text=5493884299868" */
-      href="https://wa.me/5493884299868"
+      href="https://wa.me/5493883295503"
 
 
         className="whatsapp-button">
