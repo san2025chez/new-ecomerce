4af1c16cd70b777ea9f9dc121dbce5737bf6ef68
ana <vane2013sanchez@gmail.com>
2023-08-09 13:43:00 -0300

    add-styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -43,68 +43,74 @@ console.log("CART en cart",cart);
 
 const classes = useStyles();
 return(
+ (cart.length ==!0) ? 
+   <>
+   
+     <Grid container justifyContent="center" style={{ marginTop: "20px",paddingBottom: "10px"}}>
+     <Card sx={{ minWidth: 275 }} container justifyContent="center">
+     <CardContent justifyContent="center">
+   
+   <div id="CartList" className="cart container">
+   <div className="cart__titulos container">
+     <h6 className="cart__titulos__text">Producto</h6>
+     <h6 className="cart__titulos__text">Descripción</h6>
+     <h6 className="cart__titulos__text">Cantidad</h6>
+     <h6 className="cart__titulos__text">Precio</h6>
+   </div>
+   <div>
+     {cart.map((product) => (
+        <div  key={product.id} className="container cart">
+        <div className="cart__detail container">
+          <div className="cart__img cart__items">
+            <img
+              src={product.img}
+              alt={`img-${product.id}`}
+              className="cart__img-imagen"
+            />
+          </div>
+          <div className="cart__items">
+            <h5 className="cart__item"> {product.productName} </h5>
+          </div>
+          <div className="cart__items">
+            <CartCounter inicialValue={product.quantity} product={product} />
+          </div>
+          <div className="cart__items">
+            <h6 className="cart__precio"> {product.price} </h6>
+          </div>
+          <div className="cart__items">
+            <i
+              className="far fa-trash-alt cart__eliminar"
+              onClick={() => removeItem(product.id)}></i>
+          </div>
+        </div>
+      </div>  
+     )) }  </div>
+   <div className="cart__buyTotal container">
+     <h5 className="cart__buyTotal__title ">
+       Total{" "}
+       <span className="cart__buyTotal__title--bold">${totalCompra()}</span>
+     </h5>
+     <Link to='/login'><Button  variant="outlined"  style={buttonStyle}  /*  onClick={handleCheckout} */
+       /* className={"button__agregar"} */
+       content={"Comprar"}>
+         COMPRAR
+     </Button></Link>
+   
+   </div>
+   </div>
+   </CardContent>
+    </Card>
+    </Grid>
+   
+   
+   
+   </>
+  
 
-<>
-
-  <Grid container justifyContent="center" style={{ marginTop: "20px",paddingBottom: "10px"}}>
-  <Card sx={{ minWidth: 275 }} container justifyContent="center">
-  <CardContent justifyContent="center">
-
-<div id="CartList" className="cart container">
-<div className="cart__titulos container">
-  <h6 className="cart__titulos__text">Producto</h6>
-  <h6 className="cart__titulos__text">Descripción</h6>
-  <h6 className="cart__titulos__text">Cantidad</h6>
-  <h6 className="cart__titulos__text">Precio</h6>
-</div>
-<div>
-  {cart.map((product) => (
-     <div  key={product.id} className="container cart">
-     <div className="cart__detail container">
-       <div className="cart__img cart__items">
-         <img
-           src={product.img}
-           alt={`img-${product.id}`}
-           className="cart__img-imagen"
-         />
-       </div>
-       <div className="cart__items">
-         <h5 className="cart__item"> {product.productName} </h5>
-       </div>
-       <div className="cart__items">
-         <CartCounter inicialValue={product.quantity} product={product} />
-       </div>
-       <div className="cart__items">
-         <h6 className="cart__precio"> {product.price} </h6>
-       </div>
-       <div className="cart__items">
-         <i
-           className="far fa-trash-alt cart__eliminar"
-           onClick={() => removeItem(product.id)}></i>
-       </div>
-     </div>
-   </div>  
-  )) }  </div>
-<div className="cart__buyTotal container">
-  <h5 className="cart__buyTotal__title ">
-    Total{" "}
-    <span className="cart__buyTotal__title--bold">${totalCompra()}</span>
-  </h5>
-  <Link to='/login'><Button  variant="outlined"  style={buttonStyle}  /*  onClick={handleCheckout} */
-    /* className={"button__agregar"} */
-    content={"Comprar"}>
-      COMPRAR
-  </Button></Link>
-
-</div>
-</div>
-</CardContent>
- </Card>
- </Grid>
+:
+<><h5>No hay productos</h5></>
 
 
-
-</>
 
 )
 
