79a5b358dcac6c73f8bc3d6211fc9899102881ca
ana <vane2013sanchez@gmail.com>
2023-09-01 10:57:15 -0300

    add-styles-footer

--- src/components/WhatsApp/WhatsApp.js
+++ src/components/WhatsApp/WhatsApp.js
@@ -6,10 +6,7 @@ export default function WhatsApp() {
     <>
       <a
         target="blank"
-      /*   href="https://api.whatsapp.com/send?phone=&text=5493884299868" */
       href="https://wa.me/5493883295503"
-
-
         className="whatsapp-button">
         <i className="fab fa-whatsapp"></i>
       </a>
