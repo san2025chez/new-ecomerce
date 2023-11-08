5c2cb0beb5a483843ea85503c5b6b6ae5a18f8cc
ana <vane2013sanchez@gmail.com>
2023-11-08 15:34:43 -0500

    update-search

--- src/components/NavBar1/NavBar.js
+++ src/components/NavBar1/NavBar.js
@@ -11,7 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import CartWidget from '../CartWidget/CartWidget'
 import './NavBar.scss'
 import { useState } from 'react';
@@ -20,44 +20,45 @@ import SearchIcon from '@mui/icons-material/Search';
 import axios from 'axios';
 import { APIs } from '../../constants/constants';
 import { useNavigate } from 'react-router-dom';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 const pages = [
   {
-    id:1,
+    id: 1,
     name: 'Cajas'
   },
   {
-    id:2,
+    id: 2,
     name: 'Botellines'
   },
   {
-    id:3,
+    id: 3,
     name: 'Cosmeticos'
   },
   {
-    id:4,
+    id: 4,
     name: 'DXN'
   },
 ];
 const settings = [
   {
-    id:1,
-    name:  'Profile'
+    id: 1,
+    name: 'Profile'
   },
   {
-    id:2,
+    id: 2,
     name: 'Account'
   },
   {
-    id:3,
+    id: 3,
     name: 'Dashboard',
   },
   {
-    id:4,
+    id: 4,
     name: 'Logout'
   }];
 
-const NavBar= () => {
+const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   const [searchValue, setSearchValue] = useState('');
@@ -79,20 +80,20 @@ const NavBar= () => {
     setAnchorElUser(null);
   };
 
-  const handleKeyPress = async(e) => {
+  const handleKeyPress = async (e) => {
     if (e.key === 'Enter') {
-    
-        navigate(`/search/${searchValue}`)
-        setSearchValue('')
+
+      navigate(`/search/${searchValue}`)
+      setSearchValue('')
     }
-   
+
   };
-
-
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
- 
-    <AppBar position="static" style={{backgroundColor: "#6a1b9a"}}>
+
+    <AppBar position="static" style={{ backgroundColor: "#6a1b9a" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
@@ -101,21 +102,31 @@ const NavBar= () => {
             component="div"
             sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
           >
-                <Link to="/"><h2>LEO</h2></Link>
+            <Link to="/"><h2>LEO</h2></Link>
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-                 edge="start"
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: isMobile ? 'space-between' : 'flex-start',  alignItems: 'center'  }}>
+          
+              <div style={{ display: 'flex' }}>
+           
+  <IconButton
+  edge="start"
+  size="large"
+  aria-label="account of current user"
+  aria-controls="menu-appbar"
+  aria-haspopup="true"
+  onClick={handleOpenNavMenu}
+  color="inherit"
+  style={{ marginLeft: 'auto' }}
+>
+  <MenuIcon />
+
+</IconButton>
+
+</div>
+          
+       
+            
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
@@ -137,110 +148,86 @@ const NavBar= () => {
                 width: '100%',
                 height: '100%',
               }}
-           
+
               PaperProps={{
                 style: {
                   backgroundColor: 'rgba(106, 27, 154, 0.8)', // Cambia el color de fondo a rojo
                   padding: '1rem',
-               
-               
+
+
                 },
               }}
               sx={{
                 display: { xs: 'block', md: 'none' },
               }}
             >
-              {pages.map((item) => ( 
-               <Link to={`/categoria/${ item.name }`}><MenuItem key={item.id} onClick={handleCloseNavMenu}>
+              {pages.map((item) => (
+                <Link to={`/categoria/${item.name}`}><MenuItem key={item.id} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">{item.name}</Typography>
-                  </MenuItem></Link>
+                </MenuItem></Link>
               ))}
             </Menu>
+
           </Box>
-  {/*         <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            <Link to="/">LEO</Link>
-          </Typography> */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              
+
+       <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((item, index) => (
               <Button
                 key={item.id}
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: 'blue', display: 'block' }}
               >
-               <Link to={`/categoria/${ item.name }` }>  {index !== 0 && <span style={{ marginRight: '10px' }} />} {item.name} </Link>
+                <Link to={`/categoria/${item.name}`}>  {index !== 0 && <span style={{ marginRight: '10px' }} />} {item.name} </Link>
               </Button>
             ))}
-          </Box>
-          <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto' }}>
-  <SearchIcon  style={{ color: 'white' }}   onClick={() => {
-  
-      const enterKeyPressEvent = { key: 'Enter', keyCode: 13 };
-      handleKeyPress(enterKeyPressEvent);
-    }} />
-  <InputBase
-    placeholder="Buscar..."
-    inputProps={{ 'aria-label': 'search' }}
-    value={searchValue}
-    onChange={(e) => setSearchValue(e.target.value)}
-    onKeyPress={handleKeyPress}
-    style={{
-      marginLeft: '8px',
-      backgroundColor: 'white',
-      borderRadius: '4px',
-      padding: '4px 8px',
-      color: 'black',
-    }}
-  />
-</div>
-          <Box sx={{ flexGrow: 0 }}>
-          <Tooltip title="Open settings">
-            
-              <CartWidget></CartWidget >
-                   
-              
-            </Tooltip>
-            </Box>
-      {/*     <Box sx={{ flexGrow: 0 }}>
+          </Box> 
+          <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto', position: 'relative' }}>
+            <InputBase
+              placeholder="Buscar..."
+              inputProps={{ 'aria-label': 'search' }}
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              onKeyPress={handleKeyPress}
+              style={{
+                backgroundColor: 'white',
+                borderRadius: '4px',
+                padding: '4px 8px 4px 32px',  // Agregamos espacio a la izquierda para acomodar el icono.
+                color: 'black',
+              }}
+            />
+            <SearchIcon
+              style={{
+                color: 'gray',
+                position: 'absolute',
+                left: '8px',  // Ajusta la posición izquierda según sea necesario.
+                top: '50%',   // Alinea verticalmente en el centro.
+                transform: 'translateY(-50%)',  // Alinea verticalmente en el centro.
+                cursor: 'pointer',
+              }}
+              onClick={() => {
+                const enterKeyPressEvent = { key: 'Enter', keyCode: 13 };
+                handleKeyPress(enterKeyPressEvent);
+              }}
+            />
+          </div>
+
+           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton color="primary"  style={{ color: 'rgb(255, 0, 0)' }}  onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="" />
-              </IconButton>
-              
-            </Tooltip> */}
-     
-           {/*  <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-            
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((item) => (
-                <MenuItem key={item.id} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{item.name}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
-     {/*      </Box> */}
+
+              <CartWidget></CartWidget >
+
+
+            </Tooltip>
+          </Box>
+
+
         </Toolbar>
+        
       </Container>
+      
     </AppBar>
-   
+
   );
 };
 export default NavBar;
