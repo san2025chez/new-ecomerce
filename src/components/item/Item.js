1b89effd4d3a10e1b901e440472e3e0e28d9c686
ana <vane2013sanchez@gmail.com>
2024-05-14 14:58:50 -0300

    update-card

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,174 +1,95 @@
-import {Link} from 'react-router-dom'
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-
-import { red } from '@material-ui/core/colors';
+import Typography from '@material-ui/core/Typography';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import {AddShoppingCart} from '@material-ui/icons'
-import accounting from'accounting'
-import './item.scss'
+import { BsPlus, BsEyeFill } from "react-icons/bs";
+import './item.scss';
+import { useContext } from 'react';
+import { CartCntext2 } from '../../context/CartCntext2';
 
 const useStyles = makeStyles((theme) => ({
- 
-  root: {
+  card: {
     maxWidth: 345,
-    backgroundColor: 'white', //[500],
-
-    
-  },
-  action:{
-      marginTop:"1rem",
-
+    backgroundColor: 'white',
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderRadius: 0,
+    position: 'relative', // Para posicionar los íconos sobre la imagen
   },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
     height: 150,
-    width:150,
-
-
-
-
-
- 
+    width: 150,
   },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: 'white',
-  },
-  mimargen:{
-    marginTop: "3rem",
-    
- 
-  },
-  card: {
-    [theme.breakpoints.down('sm')]: {
-      width: 180,
-      height:260,
-    },
-    [theme.breakpoints.down('xs')]: {
-      width: 180,
-      height:260,
-    
-    },
- 
-    margin:'auto',
+  iconContainer: {
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'row',
+    justifyContent: 'center', // Espacio entre los íconos
     alignItems: 'center',
+    paddingRight:'10px',
 
-    borderRadius: 0, // Configuramos el radio del borde a 0 para que sea cuadrado
-   // boxShadow: 'none', // Esto quitará la sombr
- 
+    padding: theme.spacing(2), // Espaciado entre los iconos y el borde del card
+
+
+    background: 'rgba(255, 255, 255, 0.8)', 
   },
-
+  icon: {
+    margin: theme.spacing(0, 2), // Espacio entre los iconos
+    fontSize: 20, // Tamaño de los iconos
+    backgroundColor: 'transparent', 
+  },
+  favoriteIcon: {
+    marginBottom: '4px', // Espacio entre los íconos
+  },
 }));
-const Item =({product}) => {
-    
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
 
-    const handleExpandClick = () => {
-      setExpanded(!expanded);
-    };
-    return(
-    
-        
-       
+const Item = ({ product }) => {
+  const classes = useStyles();
+  const { addItem } = useContext(CartCntext2);
 
-      <Card className={classes.card}  >
+  return (
+    <Card className={classes.card}>
       <CardHeader
-    
-    disableTypography
-        title={ <Typography style={{ marginBottom: '0.1rem' }}>{product.name}</Typography>}
-      /*   subheader={ <Typography
-          className={classes.action}
-          variant='h6'
-          color='textSecondary'
-          style={{ marginTop: '0.1rem' }}
-          sx={{ fontSize: { xs: '1rem', md: '1.5rem' } }}>
-              {accounting.formatMoney(price)}
-        
-          </Typography>} */
-
-
-    
+        disableTypography
+        title={<Typography style={{ marginBottom: '0.1rem' }}>{product.name}</Typography>}
       />
-       <Link to={`/detalle/${ product.id }`}> 
-       {
-        product.images[0].url ?
-        <CardMedia
-        className={classes.media}
-        image={product.images[0].url}
-
-        title="Paella dish"
-      />
-      : 
-      <CardMedia
-      className={classes.media}
-      image={product.images[0]}
-
-      title="Paella dish"
-    />
-
-       }</Link>
-       <Link to={`/detalle/${ product.id }`}> <CardContent>
-      {/*   <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography> */}
-      </CardContent></Link>
-     {/*  <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <AddShoppingCart fontSize='large'/>
-        </IconButton>
-        
-        {Array(4)
-        .fill()
-        .map((_,i)=> (
-            <p>&#11088;</p>
-        ))}
-        <IconButton
-          className={clsx(classes.expand, { [classes.expandOpen]: expanded,   })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
+      <Link to={`/detalle/${product.id}`}> 
+        {product.images[0].url ? (
+          <CardMedia
+            className={classes.media}
+            image={product.images[0].url}
+            title="Paella dish"
+          />
+        ) : (
+          <CardMedia
+            className={classes.media}
+            image={product.images[0]}
+            title="Paella dish"
+          />
+        )}
+      </Link>
+      <CardMedia>
+      <div className={classes.iconContainer}>
+        <button onClick={() => addItem(product, product.id)}>
+          <FavoriteIcon color="secondary" className={classes.icon}/>
+        </button>
+        <Link
+          to={`/detalle/${product.id}`}
+          className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl"
         >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions> */}
-
+          <BsEyeFill  className={classes.icon}  />
+        </Link>
+      </div>
+      </CardMedia>
     </Card>
-
-
-  
- 
-    )
+  );
 }
 
-export default Item
+export default Item;
